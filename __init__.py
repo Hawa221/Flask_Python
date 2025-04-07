3f6f1884924f2cb9a6e20c253210c3dab9adf19a
Hawa221 <hawa.doucoure1@ecoles-epsi.net>
2025-04-07 10:08:15 +0200

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -6,13 +6,15 @@ app = Flask(__name__)
 
 @app.route('/<int:valeur>')
 def exercice(valeur):
-    for i in range(1,n+1):
-       espace = ' ' * (n - i)
-       chiffres = ''
-        for j in range(1,i+1):
-            chiffres += str(j)
-        for i in range(i-1,0,-1):
-            print(espace + chiffres)
+    pyramide = ''
+    for i in range(1, valeur + 1):
+        pyramide += '&nbsp;' * (valeur - i)
+        for j in range(1, i + 1):
+            pyramide += str(j)
+        for j in range(i - 1, 0, -1):
+            pyramide += str(j)
+        pyramide += '<br>'
+    return pyramide
       
 if __name__ == "__main__":
   app.run(debug=True)
