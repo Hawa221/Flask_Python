139d99344bb29fd92f43bcc870b74c49dd8a0bfb
Hawa221 <hawa.doucoure1@ecoles-epsi.net>
2025-04-07 10:03:36 +0200

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,4 +1,4 @@
-paces =from flask import Flask
+from flask import Flask
 from flask import render_template
 from flask import json                                                                                                                                     
 app = Flask(__name__)                                                                                                                  
@@ -7,7 +7,7 @@ app = Flask(__name__)
 @app.route('/<int:valeur>')
 def exercice(valeur):
     for i in range(1,n+1):
-       espace = ' ' * (n-1)
+       espace = ' ' * (n - i)
        chiffres = ''
         for j in range(1,i+1):
             chiffres += str(j)
