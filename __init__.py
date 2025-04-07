34e9e37b16b6733221c8e36041c25ea434710318
Hawa221 <hawa.doucoure1@ecoles-epsi.net>
2025-04-07 14:57:08 +0200

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -3,20 +3,17 @@ from flask import render_template
 from flask import json                                                                                                                                     
 app = Flask(__name__)                                                                                                                  
 
-@app.route('/max', methods=['POST'])
-def trouver_max():
-    data = request.form['valeurs']
-    try:
-        liste = [int(x.strip()) for x in data.split(',')]
-    except ValueError:
-        return "Erreur : veuillez entrer uniquement des nombres séparés par des virgules."
-        
-    max_val = None
-    for val in liste:
-        if max_val is None or val > max_val:
-            max_val = val
+@app.route('/<path:valeurs>')
+def exercice(valeurs):
+    
+    liste_nombres = valeurs.split('/')
+    liste_nombres = [int(n) for n in liste_nombres]
+     max_valeur = liste_nombres[0]
+      for n in liste_nombres:
+        if n > max_valeur:
+            max_valeur = n
 
-    return f"La valeur maximale est : {max_val}"
+    return str(max_valeur)
 
 if __name__ == '__main__':
     app.run(debug=True)
