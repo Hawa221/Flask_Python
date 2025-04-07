8e35890545c08652f9a7da92f5610ef77671fde7
Hawa221 <hawa.doucoure1@ecoles-epsi.net>
2025-04-07 09:54:29 +0200

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -7,12 +7,12 @@ app = Flask(__name__)
 @app.route('/<int:valeur>')
 def exercice(valeur):
    int = ''
-    for j in range(valeur):
-        for i in range(1,j+2):
-            int += str(i)
-        for i in range(j, 0, -1);
-            int += str(i)
-        int += '<br>'
-    return int
+    for i in range(1,n+1):
+       chiffres = ''
+        for j in range(1,i+1):
+            chiffres += str(j)
+        for i in range(i-1,0,-1):
+            print(chiffres)
+      
 if __name__ == "__main__":
   app.run(debug=True)
