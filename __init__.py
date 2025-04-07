49f345a7cb21ea8fa6bfbc86733ab00d5473842c
Hawa221 <hawa.doucoure1@ecoles-epsi.net>
2025-04-07 14:10:03 +0200

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -3,18 +3,15 @@ from flask import render_template
 from flask import json                                                                                                                                     
 app = Flask(__name__)                                                                                                                  
 
-@app.route('/somme/<int:n>')
-def calcul_somme(n):
-    somme = 0
-    for i in range(1, n + 1):
-        if i % 11 == 0:
-            continue  
-        if i % 5 == 0 or i % 7 == 0:
-            somme += i
-        if somme > 5000:
-            break  
-    return f"La somme finale est : {somme}"
+@app.route('/<path:valeurs>')
+def exercice(valeurs):
+    liste_nombres = valeurs.split('/')
+    liste_nombres = [int(n) for n in liste_nombres]
+    resultat = 0
+    for n in liste_nombres:
+        resultat = resultat + n
+    return str(resultat)
 
-if __name__ == "__main__":
-    app.run(debug=True)
+if __name__ == '__main__':
+    app.run(host='0.0.0.0')
 
